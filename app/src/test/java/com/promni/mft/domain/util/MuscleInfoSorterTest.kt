0ebc72e9842542b0d09f91b56cc7a625f49cc68e
yfar <formakidov1996@gmail.com>
2025-03-14 13:47:48 +0100

    Refactor: Migrate MuscleInfoSorterTest.kt manual dependency mocking to MockK annotations

--- app/src/test/java/com/promni/mft/domain/util/MuscleInfoSorterTest.kt
+++ app/src/test/java/com/promni/mft/domain/util/MuscleInfoSorterTest.kt
@@ -5,14 +5,27 @@ import com.promni.mft.domain.model.MuscleInfo
 import com.promni.mft.domain.repository.MuscleRepository
 import com.promni.mft.domain.usecase.GetMuscleInfoUseCase
 import io.mockk.every
-import io.mockk.mockk
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
+@ExtendWith(MockKExtension::class)
 class MuscleInfoSorterTest {
+    @MockK
+    lateinit var muscleRepository: MuscleRepository
+
+    @MockK
+    lateinit var muscleInfoSorter: MuscleInfoSorter
+
+    @InjectMockKs
+    lateinit var getMuscleInfoUseCase: GetMuscleInfoUseCase
+
     private val now = System.currentTimeMillis()
     private val muscleInfoList = listOf(
         MuscleInfo(Muscle(4, "Triceps"), 0.5f, expectedRecovery = now - 25000L, 10L),
@@ -21,22 +34,27 @@ class MuscleInfoSorterTest {
         MuscleInfo(Muscle(2, "Abs"), 0.1f, expectedRecovery = now + 15000L, 10L),
         MuscleInfo(Muscle(1, "Biceps"), 0.3f, expectedRecovery = now + 5000L, 10L),
     )
-    private val muscleRepository = mockk<MuscleRepository>()
-    private val expectedOrder = listOf("Biceps", "Abs", "Calves", "Hamstrings", "Triceps")
-    private val muscleInfoSorter = MuscleInfoSorter()
-    private val getMuscleInfoUseCase = GetMuscleInfoUseCase(muscleRepository, muscleInfoSorter)
+    private val expectedOrderList = listOf(
+        MuscleInfo(Muscle(1, "Biceps"), 0.3f, expectedRecovery = now + 5000L, 10L),
+        MuscleInfo(Muscle(2, "Abs"), 0.1f, expectedRecovery = now + 15000L, 10L),
+        MuscleInfo(Muscle(3, "Calves"), 0.2f, expectedRecovery = now - 26000L, 10L),
+        MuscleInfo(Muscle(5, "Hamstrings"), 0.3f, expectedRecovery = now - 25000L, 10L),
+        MuscleInfo(Muscle(4, "Triceps"), 0.5f, expectedRecovery = now - 25000L, 10L),
+    )
 
     @Test
     fun `test muscles with expectedRecovery less than now order by name, expectedRecovery greater than now sort by recovery then by name`() =
         runTest {
             // Given
             every { muscleRepository.observeMuscles() } returns flowOf(muscleInfoList)
+            every { muscleInfoSorter.sort(muscleInfoList) } returns expectedOrderList
 
             // When
             val fetchedMuscleInfoList = getMuscleInfoUseCase.invoke().first()
 
             // Then
             val actualOrder = fetchedMuscleInfoList.map { it.muscle.name }
+            val expectedOrder = expectedOrderList.map { it.muscle.name }
             assertEquals(expectedOrder, actualOrder)
         }
 
