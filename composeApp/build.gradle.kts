4d3433b57654b39c6926128a017c0579da986b14
formakidov <formakidov1996@gmail.com>
2025-10-16 16:34:25 +0200

    Chore: Remove wasmJs target
    - Deletes all files and configurations associated with the `wasmJs` target.
    - Removes the `wasmJs` source set, including `main.kt` and `Platform.wasmJs.kt`.
    - Deletes `wasmJs` resources like `index.html` and `styles.css`.
    - Comments out the `wasmJs` block in the `composeApp/build.gradle.kts` file.
    - Removes the `yarn.lock` file associated with the JS toolchain.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -36,26 +36,6 @@ kotlin {
     }
 
     jvm("desktop")
-
-//    @OptIn(ExperimentalWasmDsl::class)
-//    wasmJs {
-//        moduleName = "composeApp"
-//        browser {
-//            val rootDirPath = project.rootDir.path
-//            val projectDirPath = project.projectDir.path
-//            commonWebpackConfig {
-//                outputFileName = "mft.js"
-//                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
-//                    static = (static ?: mutableListOf()).apply {
-//                        // Serve sources to debug inside browser
-//                        add(rootDirPath)
-//                        add(projectDirPath)
-//                    }
-//                }
-//            }
-//        }
-//        binaries.executable()
-//    }
     
     sourceSets {
 
